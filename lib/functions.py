2f50ea0645c96f87c48fd9fb5ad87bfb097e84f5
daniel <danielmutisya203@gmail.com>
2023-05-17 18:12:46 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print("Hello, {}!".format(name))
+
+greet("Guido")
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, {}!".format(name))
+
+# Example usage
+greet_with_default()  # Output: Hello, programmer!
+greet_with_default("John")  # Output: Hello, John!
+
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    return result
+
 
 def halve(number):
-    pass
+    result = number / 2
+    return result
